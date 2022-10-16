12c9352b123debfbdd1f962ea01ab7f85e8e62e3
roolloo <62914628+roolloo@users.noreply.github.com>
2022-10-16 22:21:42 +0300

    pf

---
+++ js/index2.js
@@ -0,0 +1,14 @@
+
+
+// setInterval(() => {
+// 	let height = window.innerHeight
+// 	// let width = window.innerWidth
+// 	let conteiner = document.querySelector('.conteiner')
+//    // let body = document.body
+// 	conteiner.style.height = height + 'px'
+// 	// body.style.width = width + 'px'
+// 	// console.log(height)
+// }, 0)
+
+let btn = document.querySelector('.password__hide')
+
---
+++ js/index3.js
@@ -0,0 +1,11 @@
+
+
+setInterval(() => {
+	let height = window.innerHeight
+	// let width = window.innerWidth
+	let conteiner = document.querySelector('.conteiner')
+   // let body = document.body
+	conteiner.style.height = height + 'px'
+	// body.style.width = width + 'px'
+	// console.log(height)
+}, 0)
